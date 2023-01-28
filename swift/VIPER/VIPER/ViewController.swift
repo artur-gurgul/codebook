950560b2cff50d7c05b61d00aef22f0607d0a83a
Artur Gurgul <artur.gurgul@redge.com>
2023-01-28 21:31:02 +0100

    Add empty project that will consist the VIPER design pattern

---
+++ swift/VIPER/VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by Artur Gurgul on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
